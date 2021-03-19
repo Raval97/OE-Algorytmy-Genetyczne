9af1109d59c852b573fba3d7cd2cdabea955c451
Gęgotek <rafal.gegotek@comarch.com>
2021-03-19 20:08:51 +0100

    xd

--- src/main/java/algorytmyGenetyczne/Main.java
+++ src/main/java/algorytmyGenetyczne/Main.java
@@ -1,19 +1,38 @@
 package algorytmyGenetyczne;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        BorderPane mainPane = FXMLLoader.load(getClass().getResource("/ManView.fxml"));
+        Pane mainPane = FXMLLoader.load(getClass().getResource("/ManView.fxml"));
         Scene scene = new Scene(mainPane);
         stage.setScene(scene);
         stage.setTitle("Projekt");
         stage.show();
+
+
+
+        Button start = (Button) scene.lookup("#start");
+
+        System.out.println("dsf");
+        start.setText("sdfdfds");
+        start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                ComboBox m_mutacji = (ComboBox) scene.lookup("#m_mutacji");
+                String mutacja = m_mutacji.getValue().toString();
+                System.out.println(mutacja);
+            }
+        });
     }
 
     public static void main(String[] args) {
