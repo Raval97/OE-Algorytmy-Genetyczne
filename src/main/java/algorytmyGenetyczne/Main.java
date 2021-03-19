f521ac3f0a88eb540ee2a3e31d4e51ea58641923
Gęgotek <rafal.gegotek@comarch.com>
2021-03-19 18:17:49 +0100

    Initial commit

---
+++ src/main/java/algorytmyGenetyczne/Main.java
@@ -0,0 +1,22 @@
+package algorytmyGenetyczne;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane mainPane = FXMLLoader.load(getClass().getResource("/ManView.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setTitle("Projekt");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module Algorytmy_Genetyczne {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    exports algorytmyGenetyczne to javafx.graphics;
+}
